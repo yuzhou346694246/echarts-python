90e4170e09ef07b9166b0410c212ec56adc77a54
yuzhou346694246 <442704997@qq.com>
2017-02-07 12:02:19 +0800

    Update __init__.py
    gbk问题

--- echarts/__init__.py
+++ echarts/__init__.py
@@ -91,8 +91,9 @@ class Echart(Base):
         return json
 
     def _html(self):
-        with open(os.path.join(os.path.dirname(__file__), 'plot.j2')) as f:
+        with open(os.path.join(os.path.dirname(__file__), 'plot.j2'),'rb') as f:
             template = f.read()
+            template = template.decode('utf-8')
             return template.replace('{{ opt }}', json.dumps(self.json, indent=4))
 
     def plot(self, persist=True):
